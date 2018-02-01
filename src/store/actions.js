3faa8c12b976c1287d9bb042fd80d459c33d257c
Pedro Câmara <camara-pedro@hotmail.com>
2018-02-01 13:19:33 +0000

    Grouping of schedules

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,7 @@
 import mapValues from 'lodash/mapValues';
+import groupBy from 'lodash/groupBy';
 import Vue from 'vue';
 import * as mutationTypes from './mutation-types';
-
-const testdata = require('../../testdata.json');
 
 export function fetchYears({ commit }) {
   commit(mutationTypes.SET_YEARS_LOADING, true);
@@ -19,7 +18,7 @@ export function fetchSchools({ commit }) {
 // eslint-disable-next-line no-unused-vars
 export function fetchProgrammes({ commit }, schoolId) { // TODO
   commit(mutationTypes.SET_PROGRAMMES_LOADING, true);
-  return Vue.axios.get('/courses')
+  return Vue.axios.get('courses')
     .then(({ data }) => commit(mutationTypes.SET_PROGRAMMES,
       data.sort((a, b) => a.name.localeCompare(b.name)))); // TODO
 }
@@ -31,10 +30,26 @@ export function setSchool({ commit, dispatch }, schoolId) {
 
 
 // eslint-disable-next-line no-unused-vars
-function fetchProgrammeData(programme) {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(testdata['FEUP-MIEIC'] || null), 2000);
+async function fetchProgrammeData(programme) {
+  const [coursesAll, schedulesAll] = await Promise.all([
+    Vue.axios.get('course-units'),
+    Vue.axios.get('schedules'),
+  ]).then(responses => responses.map(response => response.data));
+
+  const courses = coursesAll.filter(course => course.course_id === programme.id);
+  const coursesObj = courses.reduce((obj, c) => ({ ...obj, [c.id]: true }), {});
+  const schedules = schedulesAll.filter(({ course_unit_id }) => coursesObj[course_unit_id]);
+  const schedulesGrouped = groupBy(schedules, 'course_unit_id');
+  const result = courses.map((course) => {
+    const courseLessons = schedulesGrouped[course.id] || [];
+    return ({
+      ...course,
+      lectures: courseLessons.filter(l => l.lesson_type === 'T'),
+      praticals: courseLessons.filter(l => l.lesson_type !== 'T'),
+    });
   });
+  console.log(result);
+  return result;
 }
 
 export function getScheduleData({ commit, state }, programme) {
